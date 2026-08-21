8cc63d10a7ef9c6601b461abb98d29ce81e6d82d
Jia <madeleinenakasone0926223@gmail.com>
2026-08-21 08:38:01 +0800

    chore: bump preview version to 0.1.1

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -6,7 +6,7 @@ use std::str::FromStr;
 use crate::error::{Error, Result};
 
 pub const ADAPTER_SCHEMA: &str = "keysmith-switch/adapter-v1";
-pub const APP_VERSION: &str = "0.1.0";
+pub const APP_VERSION: &str = "0.1.1";
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
